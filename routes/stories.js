191ba33ab34a9e95893ea806a288e0476e690062
“Talha-Mazar” <ahmadnutt9111@gmail.com>
2024-04-09 01:21:21 +0500

    read more from certain user added

--- routes/stories.js
+++ routes/stories.js
@@ -107,8 +107,6 @@ router.put('/:id', ensureAuth, async (req, res) => {
     }
 })
 
-module.exports = router
-
 // @desc   Delete Story
 // @route   DELETE /stories/:id
 router.delete('/:id', ensureAuth, async (req, res) => {
@@ -120,3 +118,23 @@ router.delete('/:id', ensureAuth, async (req, res) => {
         return res.render('error/500')
     }
 })
+
+// @desc   user stories
+// @route   GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public',
+        })
+            .populate('user')
+            .lean()
+
+        res.render('stories/index', { stories })
+    } catch (err) {
+        console.log(err)
+        return res.render('error/500')
+    }
+})
+
+module.exports = router
